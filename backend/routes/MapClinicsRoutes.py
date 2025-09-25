f879ac9fef2e17dec32f18df2f2574f3cb598037
alejandraoshea <alejandra.osheafernandez@usp.ceu.es>
2025-09-25 17:17:23 +0200

    updated frontend mapclinics

--- backend/routes/MapClinicsRoutes.py
+++ backend/routes/MapClinicsRoutes.py
@@ -4,9 +4,8 @@ from dotenv import load_dotenv
 import os
 from supabase import create_client
 
-load_dotenv()  # <-- important! must come before using os.getenv
-
 load_dotenv()
+
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
