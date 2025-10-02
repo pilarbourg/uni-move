244b142da6f2c0604214268a00acae688da4a9ce
alejandraoshea <alejandra.osheafernandez@usp.ceu.es>
2025-10-02 21:56:41 +0200

    added illness routes to app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from backend.routes.chat_routes import chat_routes
 from backend.routes.bio_profile_routes import bio_profile_routes
+from backend.routes.illnesses_routes import illness_routes
 from backend.routes.map_clinics_routes import map_clinics_routes
 from backend.routes.map_universities_routes import map_universities_routes
 from backend.routes.login_routes import login_routes
@@ -14,6 +15,7 @@ CORS(app)
 
 app.register_blueprint(chat_routes)
 app.register_blueprint(bio_profile_routes)
+app.register_blueprint(illness_routes)
 app.register_blueprint(map_clinics_routes)
 app.register_blueprint(map_universities_routes)
 app.register_blueprint(login_routes)
